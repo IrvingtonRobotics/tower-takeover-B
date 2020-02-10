333911e52b26b7bbfba4ca8e53f7d5c50c82a2f2
3yecup <vicky.pham92@gmail.com>
2020-02-09 18:43:16 -0800

    Port to VCS

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
+// VEX V5 C++ Project with Competition Template
 #include "vex.h"
 using namespace vex;
 
 //#region config_globals
-vex::brain      Brain;
-vex::motor      right_drive(vex::PORT1, vex::gearSetting::ratio18_1, true);
-vex::motor      left_drive(vex::PORT12, vex::gearSetting::ratio18_1, false);
-vex::motor      tilt(vex::PORT3, vex::gearSetting::ratio36_1, true);
-vex::motor      right_intake(vex::PORT4, vex::gearSetting::ratio18_1, true);
-vex::motor      left_intake(v2ex::PORT5, vex::gearSetting::ratio18_1, false);
-vex::motor      right_flywheel(vex::PORT6, vex::gearSetting::ratio18_1, true);
-vex::motor      left_flywheel(vex::PORT11, vex::gearSetting::ratio18_1, false);
+brain      Brain;
+motor      right_drive(PORT1, gearSetting::ratio18_1, true);
+motor      left_drive(PORT12, gearSetting::ratio18_1, false);
+motor      tilt(PORT3, gearSetting::ratio36_1, true);
+motor      right_intake(PORT4, gearSetting::ratio18_1, true);
+motor      left_intake(PORT5, gearSetting::ratio18_1, false);
+motor      right_flywheel(PORT6, gearSetting::ratio18_1, false);
+motor      left_flywheel(PORT11, gearSetting::ratio18_1, true);
+controller con(controllerType::primary);
+//#endregion config_globals
+int i = 1;
 
 
-vex::competition Competition;
+competition Competition;
 
-void pre_auton() {
+//void pre_auton() {
   
-
-}
 
 void autonomous() {
    
@@ -36,7 +38,7 @@ void drivercontrol() {
         //drive
         left_drive.spin(directionType::fwd, (con.Axis3.value() + con.Axis4.value())/2, velocityUnits::pct);
         //left joystick left drive
-        right_drive.spin(directionType::fwd, (con.Axis2.value())/2, velocityUnits::pct);
+        right_drive.spin(directionType::fwd, (con.Axis2.value() - con.Axis1.value())/2, velocityUnits::pct);
         //right joystick right drive
         
         //rail tilt
@@ -44,7 +46,7 @@ void drivercontrol() {
             tilt.spin(directionType::fwd, tiltSpeedPCT, velocityUnits::pct);
         }
         else if (con.ButtonB.pressing()){ //b back
-            tilt.spin.(directionType::rev, tiltSpeedPCT, velocityUnits::pct);
+            tilt.spin(directionType::rev, tiltSpeedPCT, velocityUnits::pct);
         }
         else { //none
             tilt.stop(brakeType::coast);
@@ -66,17 +68,17 @@ void drivercontrol() {
         
         //flywheel
         if(con.ButtonL1.pressing()){ //left upper shoulder full speed
-            left_flywheel.spin(directionType::fwd, flywheelSpeedPCT, velocityUnits:pct);
+            left_flywheel.spin(directionType::fwd, flywheelSpeedPCT, velocityUnits::pct);
             right_flywheel.spin(directionType::fwd, flywheelSpeedPCT, velocityUnits::pct);
         }
-        else {
+        else { 
             left_flywheel.stop(brakeType::brake);
             right_flywheel.stop(brakeType::brake);
         }
-        }
 
     }
-}
+  }
+
 
 int main() {
     // Do not adjust the lines below
@@ -84,10 +86,7 @@ int main() {
     // Set up (but don't start) callbacks for autonomous and driver control periods.
     Competition.autonomous(autonomous);
     Competition.drivercontrol(drivercontrol);
-
-    // Run the pre-autonomous function.
-    pre_auton();
-
-    // Robot Mesh Studio runtime continues to run until all threads and
-    // competition callbacks are finished.
+  
+  
 }
+
